2264d5c3441603e3aa823f97cdda7244374334ee
Vaishnavi22Thakur <vaaishnavi4713@gmail.com>
2026-08-20 14:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (66.37%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -7,38 +7,15 @@ public:
       vector<int>result;
       arr1.push_back(nums[0]);
       arr2.push_back(nums[1]);
-      int j=0,k=0;
       for(int i=2;i<n;i++){
-        if(i%2==0){
-            if(arr1[j]>arr2[k]){
-                arr1.push_back(nums[i]);
-                j++;
-            }
-            else{
-                arr2.push_back(nums[i]);
-                k++;
-            }
+        if(arr1.back()>arr2.back()){
+            arr1.push_back(nums[i]);
         }
         else{
-            if(arr2[k]>arr1[j]){
-                arr2.push_back(nums[i]);
-                k++;
-            }
-            else{
-                arr1.push_back(nums[i]);
-                j++;
-            }
+            arr2.push_back(nums[i]);
         }
       }
-      j=0,k=0;
-      while(j<arr1.size()){
-        result.push_back(arr1[j]);
-        j++;    
-      }
-      while(k<arr2.size()){
-        result.push_back(arr2[k]);
-        k++;
-      }
-      return result;
+      arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+      return arr1;
     }
 };
